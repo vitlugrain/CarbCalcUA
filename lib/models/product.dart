420215e8e8fdbecfaca56f893b59e380cbcee4b6
vitlugrain <vit.lugrain@gmail.com>
2026-09-07 10:49:43 +0300

    Decode HTML entities in product names

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -37,8 +37,8 @@ class Product {
 
   factory Product.fromJson(Map<String, dynamic> j) => Product(
         id: '${j['id']}',
-        name: '${j['name']}',
-        category: '${j['category']}',
+        name: _decodeHtmlEntities('${j['name']}'),
+        category: _decodeHtmlEntities('${j['category']}'),
         carbs: (j['carbs'] as num).toDouble(),
         protein: ((j['protein'] ?? 0) as num).toDouble(),
         fat: ((j['fat'] ?? 0) as num).toDouble(),
@@ -46,7 +46,7 @@ class Product {
         calories: ((j['calories'] ?? 0) as num).toDouble(),
         state: j['state'] ?? 'raw',
         barcode: _stringOrNull(j['barcode']),
-        manufacturer: _stringOrNull(j['manufacturer']),
+        manufacturer: _decodedStringOrNull(j['manufacturer']),
         source: _stringOrNull(j['source']),
         updatedAt: _stringOrNull(j['updatedAt'] ?? j['updated_at']),
         gramsPerPiece: _numberOrNull(j['gramsPerPiece'] ?? j['grams_per_piece']),
@@ -76,6 +76,22 @@ class Product {
     if (value == null) return null;
     if (value is num) return value.toDouble();
     return double.tryParse(value.toString().replaceAll(',', '.'));
+  }
+
+  static String _decodeHtmlEntities(String value) {
+    return value
+        .replaceAll('&quot;', '"')
+        .replaceAll('&#34;', '"')
+        .replaceAll('&#39;', "'")
+        .replaceAll('&apos;', "'")
+        .replaceAll('&amp;', '&')
+        .replaceAll('&lt;', '<')
+        .replaceAll('&gt;', '>');
+  }
+
+  static String? _decodedStringOrNull(dynamic value) {
+    final s = _stringOrNull(value);
+    return s == null ? null : _decodeHtmlEntities(s);
   }
 
   static String? _stringOrNull(dynamic value) {
